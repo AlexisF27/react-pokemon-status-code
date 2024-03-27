af1dd8c8be2817359c14a892552246cde3ad11dc
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-27 17:29:59 +0000

    Change size of subtitle

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ function NavBar() {
         ></img>
         <Typography variant="h2" style={{ fontFamily: 'Roboto' }} className="roboto-font underline-color title">HTTP POKEMON</Typography>
       </Box>
-      <Typography variant="h4" style={{ fontFamily: 'Roboto' }} className="roboto-font normal-weight description">Dogs for every HyperText Transfer Protocol response status code.</Typography>
+      <Typography variant="h5" style={{ fontFamily: 'Roboto' }} className="roboto-font normal-weight description">Dogs for every HyperText Transfer Protocol response status code.</Typography>
     </header>
   );
 }
