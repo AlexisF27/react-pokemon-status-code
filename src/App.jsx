babd5795100eacaf103b3a331046ca10d491ace2
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-27 17:36:58 +0000

    Fix margin big box with img

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 
+import { Box, Typography } from '@mui/material';
+
 import NavBar from './components/NavBar'
 import { ThemeProvider } from '@mui/material/styles';
 import fondTheme from './themes/FontTheme';
@@ -9,6 +11,8 @@ function App() {
   return (
     <ThemeProvider theme={fondTheme} >
       <NavBar/>
+      <Typography variant="h5" style={{ fontFamily: 'Roboto' }} className="roboto-font normal-weight description">Dogs for every HyperText Transfer Protocol response status code.</Typography>
+
 
 
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,16 +6,16 @@ import { Box, Typography } from '@mui/material';
 function NavBar() {
   return (
     <header id="header">
+      <img
+        id="img-logo"
+        className="brightness"
+        src="https://elcandelerotecnologico.com/wp-content/uploads/2017/03/juegos_logo_pokemon.jpg"
+        alt="logo"
+      ></img>
       <Box className="box-img-title">
-        <img
-          id="img-logo"
-          className="brightness"
-          src="https://elcandelerotecnologico.com/wp-content/uploads/2017/03/juegos_logo_pokemon.jpg"
-          alt="logo"
-        ></img>
+
         <Typography variant="h2" style={{ fontFamily: 'Roboto' }} className="roboto-font underline-color title">HTTP POKEMON</Typography>
       </Box>
-      <Typography variant="h5" style={{ fontFamily: 'Roboto' }} className="roboto-font normal-weight description">Dogs for every HyperText Transfer Protocol response status code.</Typography>
     </header>
   );
 }
