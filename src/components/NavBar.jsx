0c94bacd13895df6a6230861ea9d1f16f9979538
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-03-27 00:58:22 +0000

    Header - logo - title

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import './navBar.css'
+function NavBar() {
+  return (
+    <header id='header'>
+      <img
+        id="img-logo"
+        src="https://private-user-images.githubusercontent.com/31175897/317072066-79043652-7f04-403c-802f-5d32b08929d0.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTE0OTYxOTksIm5iZiI6MTcxMTQ5NTg5OSwicGF0aCI6Ii8zMTE3NTg5Ny8zMTcwNzIwNjYtNzkwNDM2NTItN2YwNC00MDNjLTgwMmYtNWQzMmIwODkyOWQwLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMjYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzI2VDIzMzEzOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWRhOGVlMWY2NDg3MmRkNmUwYjllNmE0OWU0Mzk0YmM1MjI3MDZmMDM2YTk1M2YxMjdlMGY0ODdiMGUyOTFkZWImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.MvkdEv2Olklrk9hNvtmdtnCr1BftEMW092FANP1AEzA"
+        alt="logo"
+      ></img>
+      <h1>
+        HTTP STATUS POKEMON</h1></header>
+
+
+  )
+}
+
+export default NavBar
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import './index.css'
 
 import App from './App.jsx'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
