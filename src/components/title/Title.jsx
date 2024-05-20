4f2d4a61b0804965a98b10ce78eb56189ccf6d7d
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-05-20 01:29:56 +0100

    Install tailwind/title tailwind

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -1,10 +1,9 @@
-import './title.css';
-
 import { Box, Typography } from '@mui/material';
 
 function Title() {
   return (
-    <Box className="title-box">
+    <Box className="flex justify-center align-center">
+      
       <Typography variant="h5" style={{ fontFamily: 'Roboto' }} className="roboto-font normal-weight description">Dogs for every HyperText Transfer Protocol response status code.</Typography>
     </Box>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./main.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
