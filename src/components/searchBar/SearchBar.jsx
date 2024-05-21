07941ee2d14cc22a176d0d743c734623542bf71c
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-05-21 23:30:45 +0100

    Search bar into the nav

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,18 +1,19 @@
-import { Box, TextField } from '@mui/material';
+import { Box, Input } from '@mui/material';
+
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
   return (
     <Box className="flex flex-col">
 
-      <Box className="flex justify-center items-center pt-10">
-        <TextField
-          InputProps={{
-            readOnly: false,
-          }}
+      <Box className="flex justify-center items-center pt-10 pr-10">
+        <Input
           className="w-3/4 ml-2.5"
-          defaultValue="https://http.dog/[code].jpg"
+          placeholder="Search"
           type='search'
-        />
+        />  
+
+        <SearchIcon/>
 
       </Box>
     </Box>
--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from '@mui/material';
 
 function Title() {
   return (
-    <Box className="flex justify-center align-center">
+    <Box className="flex justify-center align-center pt-10">
       <Typography variant="h5" className=" description">Pokemons for every HyperText Transfer Protocol response status code.</Typography>
     </Box>
   )
