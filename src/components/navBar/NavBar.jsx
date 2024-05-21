53fa7dcf52ff12ec0cc7cd230eae3ba5004e2b90
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-05-21 02:15:06 +0100

    Update navBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -5,15 +5,14 @@ import { Box, Typography } from '@mui/material';
 
 function NavBar() {
   return (
-    <header className="flex flex-row items-center justify-center grow-2 flex-wrap">
+    <header className="flex flex-row items-center justify-center grow-2 flex-wrap pt-10 pb-10">
       <img
-        id="img-logo"
-        className="brightness"
+        className="h-20 w-30 pr-10"
         src="https://elcandelerotecnologico.com/wp-content/uploads/2017/03/juegos_logo_pokemon.jpg"
         alt="logo"
       ></img>
       <Box className="flex flex-row">
-        <Typography variant="h2" style={{ fontFamily: 'Roboto' }} className="roboto-font ">HTTP POKEMON</Typography>
+        <Typography variant="h2" >HTTP POKEMON</Typography>
       </Box>
     </header>
   );
