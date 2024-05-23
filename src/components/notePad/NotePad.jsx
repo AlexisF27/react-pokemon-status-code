01784e1c7a1035a810a361aed78a37e261f1b4bf
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-05-23 22:41:46 +0100

    the search bar filters the json by the status code value

--- src/components/notePad/NotePad.jsx
+++ src/components/notePad/NotePad.jsx
@@ -2,27 +2,41 @@ import { Box, Container, Grid } from "@mui/material"
 import { useEffect, useState } from 'react';
 
 import PokemonStatusCodeCard from "../pokemonStatusCodeCard/PokemonStatusCodeCard"
+import PropTypes from 'prop-types'
 import statusCodes from '../../assets/data/status_code_pokemon.json';
 
-function NotePad() {
+function NotePad({ inputSearch }) {
   const [pokemonStatusCodeCard, setPokemonStatusCodeCard] = useState([]);
 
   useEffect(() => {
-    setPokemonStatusCodeCard(statusCodes);
-  }, []);
+    if (inputSearch) {
+      const searchQuery = Number(inputSearch);
+      const filteredStatusCodes = statusCodes.filter(statusCodeCard =>
+        statusCodeCard.status_code_id === searchQuery
+      );
+      setPokemonStatusCodeCard(filteredStatusCodes);
+    } else {
+      setPokemonStatusCodeCard(statusCodes);
+    }
+  }, [inputSearch]);
 
   return (
     <Container className="ml-16 pt-16">
       <Box className="flex justify-center items-center" >
         <Grid container spacing={2}>
           {pokemonStatusCodeCard.map((statusCodeCard) => ((
-            <PokemonStatusCodeCard key={statusCodeCard.status_code_id} statusCodeCard={statusCodeCard}/>
+            <PokemonStatusCodeCard key={statusCodeCard.status_code_id} statusCodeCard={statusCodeCard} />
           )))}
         </Grid>
 
       </Box>
     </Container>
   )
+}
+
+NotePad.propTypes = {
+  inputSearch: PropTypes.string.isRequired,
+
 }
 
 export default NotePad
