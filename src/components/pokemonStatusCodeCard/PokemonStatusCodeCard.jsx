1f0306493dc02aad228e3ce654ef58f439490c81
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-05-28 14:29:36 +0100

    Generic img to the CardMedia h-56

--- src/components/pokemonStatusCodeCard/PokemonStatusCodeCard.jsx
+++ src/components/pokemonStatusCodeCard/PokemonStatusCodeCard.jsx
@@ -1,6 +1,7 @@
 import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material"
 
 import PropTypes from 'prop-types'
+import img from '../../assets/img/logo.jpg';
 
 function PokemonStatusCodeCard({ statusCodeCard }) {
   return (
@@ -9,7 +10,9 @@ function PokemonStatusCodeCard({ statusCodeCard }) {
         <Card className="m-2 border-black h-72 w-80">
           <CardActionArea>
             <CardMedia 
+              className="h-56"
               title={statusCodeCard.title}
+              image={img}
             />
             <CardContent>
               <Typography className="flex justify-center" gutterBottom variant="h5" component="div">
