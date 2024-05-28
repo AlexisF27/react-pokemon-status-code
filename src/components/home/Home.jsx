f4be90ccd78149c767be2e69667163504eed4631
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-05-28 14:57:44 +0100

    Create Home component to wrap other components

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,20 @@
+import LinkCode from '../linkCode/LinkCode';
+import NotePad from '../notePad/NotePad';
+import PropTypes from 'prop-types'
+import Title from '../title/Title';
+
+function Home({inputSearch}) {
+  return (
+    <>
+      <Title />
+      <LinkCode />
+      <NotePad inputSearch={inputSearch} />
+    </>
+  )
+}
+
+Home.propTypes = {
+  inputSearch: PropTypes.string,
+}
+
+export default Home
