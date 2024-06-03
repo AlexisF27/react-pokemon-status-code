66dc81111126231d980a5791de447edc96a7da79
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-06-03 17:46:19 +0100

    Singular Fetch & variables fetched

--- src/components/statusCodeDetail/StatusCodeDetail.jsx
+++ src/components/statusCodeDetail/StatusCodeDetail.jsx
@@ -1,26 +1,34 @@
+import { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 
 import { Box } from "@mui/material"
 import ButtonsAfterBeforeStatusCode from '../buttonsAfterBeforeStatusCode/ButtonsAfterBeforeStatusCode';
 import StatusCodeDetailTypography from '../statusCodeDetailTypography/StatusCodeDetailTypography';
+import statusCodes from '../../assets/data/status_code_pokemon.json';
 
 function StatusCodeDetail() {
   const { statusCodeId } = useParams();
   const location = useLocation();
   const { img, statusCodeTitle } = location.state || {};
-  console.log(statusCodeTitle);
+  const  [singularPokemonStatusDetail, setSingularPokemonStatusDetail] = useState("");
 
+  useEffect(() => {
+    const singularPokemonStatusDetail = Object.values(statusCodes).find(statusCode => statusCode.status_code_id === parseInt(statusCodeId));
+    setSingularPokemonStatusDetail(singularPokemonStatusDetail);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log(singularPokemonStatusDetail);
   return (
     <>
       <Box className="flex justify-center items-center pt-12">
         <Box className="w-2/5 h-2/5">
-          <img src={img} alt="status code" />
+          <img src={singularPokemonStatusDetail.img_status_code || img} alt="status code" />
           <Box className="flex justify-center pt-2">
             <ButtonsAfterBeforeStatusCode/>
           </Box>
         </Box>
         <Box className="pl-16 w-2/5">
-          <StatusCodeDetailTypography statusCodeId={statusCodeId} statusCodeTitle={statusCodeTitle} />
+          <StatusCodeDetailTypography statusCodeId={singularPokemonStatusDetail.status_code_id || statusCodeId} statusCodeTitle={singularPokemonStatusDetail.title || statusCodeTitle} />
         </Box>
       </Box>
 
