5ffb9735de5321193c473bf1d0fac3173d0ab0d5
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-07-11 20:19:53 +0100

    Add to tawilwind js background color

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -2,16 +2,17 @@ import { Box, Typography } from '@mui/material';
 
 function NavBar() {
   return (
-    <header className="flex flex-row items-start justify-center grow-2 flex-wrap pb-6 pt-6 border-b-2 bg-red-500">
-      <img
-        className="h-20 w-30 pr-10"
-        src="https://elcandelerotecnologico.com/wp-content/uploads/2017/03/juegos_logo_pokemon.jpg"
-        alt="logo"
-      ></img>
-      <Box className="flex flex-row">
-        <Typography variant="h2" >HTTP POKEMON</Typography>
-      </Box>
-    </header>
+    <Box>
+      <header className="flex flex-row items-start justify-center grow-2 flex-wrap pb-2 pt-4 border-b-2 bage_backgroundColor">
+        <img
+          className="h-20 w-30 pr-10"
+          src="https://elcandelerotecnologico.com/wp-content/uploads/2017/03/juegos_logo_pokemon.jpg"
+          alt="logo"></img>
+        <Box className="flex flex-row">
+          <Typography variant="h2">HTTP POKEMON</Typography>
+        </Box>
+      </header>
+    </Box>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 export default {
-  content: [
-    "./main.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./main.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      backgroundColor: {
+        bage_backgroundColor: 'rgba(255, 245, 214, 0.8)',
+      },
     },
   },
   plugins: [],
-}
-
+};
